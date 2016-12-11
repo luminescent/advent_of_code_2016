8a244243ba87549952bdb4a1ebc8ac9af1d68aa9
luminescent <christina.androne@gmail.com>
2016-12-11 21:48:52 +0000

    day 9

---
+++ advent_of_code_2016/advent_of_code_2016/Day9.fs
@@ -0,0 +1,52 @@
+﻿module Day9
+
+open System 
+open System.IO
+
+type Marker = { Length: int; Times: int}
+
+let get_marker_values (input:string) openingBracket closingBraket = 
+    let marker_parts = 
+        input.Substring(openingBracket + 1, closingBraket - openingBracket - 1).Split([| 'x' |], StringSplitOptions.RemoveEmptyEntries)
+    { Length = marker_parts.[0] |> Int32.Parse; Times = marker_parts.[1] |> Int32.Parse }
+        
+
+let rec get_decompress_length (input: string) currentPosition currentLength = 
+    match currentPosition = input.Length with 
+    | true -> currentLength
+    | _ -> 
+        match input.Chars(currentPosition) with 
+        | '(' -> 
+            let closingBracket = input.IndexOf(')', currentPosition)
+            let marker = get_marker_values input currentPosition closingBracket
+            get_decompress_length input (closingBracket + marker.Length + 1) (currentLength + marker.Length * marker.Times)
+        | ' ' ->
+            get_decompress_length input (currentPosition + 1) currentLength // not sure where space appears!
+        | _ -> 
+            get_decompress_length input (currentPosition + 1) (currentLength + 1)
+
+
+let rec get_decompress_length2 (input: string) currentPosition (currentLength:float) = 
+    match currentPosition = input.Length with 
+    | true -> currentLength
+    | _ -> 
+        match input.Chars(currentPosition) with 
+        | '(' -> 
+            let closingBracket = input.IndexOf(')', currentPosition)
+            let marker = get_marker_values input currentPosition closingBracket
+            let repeated_pattern = input.Substring(closingBracket + 1, marker.Length)
+            let repeated_pattern_decompressed_length = get_decompress_length2 repeated_pattern 0 0.0
+            get_decompress_length2 input (closingBracket + marker.Length + 1) (currentLength + repeated_pattern_decompressed_length * (float)marker.Times)
+        | ' ' ->
+            get_decompress_length2 input (currentPosition + 1) currentLength // not sure where space appears!
+        | _ -> 
+            get_decompress_length2 input (currentPosition + 1) (currentLength + 1.0)
+
+let run_day9() =
+    let input = File.ReadAllText("Day9.txt")
+
+    let length = get_decompress_length input 0 0 
+    printfn "Total lenght: %A" length 
+
+    let inner_markers_decompressing_length = get_decompress_length2 input 0 0.0 
+    printfn "Total length: %A" inner_markers_decompressing_length
--- advent_of_code_2016/advent_of_code_2016/Program.fs
+++ advent_of_code_2016/advent_of_code_2016/Program.fs
@@ -9,6 +9,7 @@ open Day5
 open Day6
 open Day7
 open Day8
+open Day9
 open Microsoft.FSharp.Core.Operators 
 open System
 
@@ -24,12 +25,12 @@ let main argv =
     //run_day5()
     //run_day6()
     //run_day7()
-    run_day8()
-//
-//    let ia = parse_IpAddress "aba[bab]xyz"
-//
-//    ["aba[bab]xyz"; "xyx[xyx]xyx"; "aaa[kek]eke"; "zazbz[bzb]cdb"]
-//    |> List.iter (fun addr -> printfn "%A" (addr |> parse_IpAddress |> supports_ssl) )
+    //run_day8()
+    run_day9()
+
+
+//    ["(3x3)XYZ"; "X(8x2)(3x3)ABCY"; "(27x12)(20x12)(13x14)(7x10)(1x12)A"; "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"]
+//    |> List.iter (fun input -> printfn "%A" (get_decompress_length2 input 0 0) )
 
     
 
